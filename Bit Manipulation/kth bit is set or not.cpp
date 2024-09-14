26d5e36df1ff7887a12f1faf104e21d81f37b2a6
Shivam-thakur05 <bambamthakur6202@gmail.com>
2024-09-14 16:26:33 +0530

    Updated using right shift

--- Bit Manipulation/kth bit is set or not.cpp
+++ Bit Manipulation/kth bit is set or not.cpp
@@ -7,3 +7,14 @@ class Solution {
         
     }
 };
+
+
+class Solution {
+  public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k) {
+        if((1 & (n>>k)) != 0) return 1;
+        else return 0;
+        
+    }
+};
