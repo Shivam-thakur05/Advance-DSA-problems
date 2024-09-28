4fb4c229c67c7cede0810046f1dd64f2998e9cf9
Shivam-thakur05 <bambamthakur6202@gmail.com>
2024-09-28 17:23:37 +0530

    Dp

---
+++ Dynamic Programming/minimum cost climbing stairs.cpp
@@ -0,0 +1,47 @@
+//memoization method
+
+class Solution {
+private:
+    int solve(vector<int> &cost, int n, vector<int> &dp){
+        if(n == 0 || n == 1) return cost[n];
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = cost[n] + min(solve(cost, n-1,dp),solve(cost, n-2,dp));
+        return dp[n];
+
+    }
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n+1, -1);
+        int ans = min(solve(cost, n-1,dp),solve(cost, n-2,dp));
+        return ans;
+    }
+};
+
+
+//second method:- tabulation method
+class Solution {
+private:
+    int solve(vector<int> &cost, int n){
+        //create a dp
+        vector<int>dp(n+1);
+        //add the base case value into dp
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        //for rest of them using loop to iterate
+
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+       return min(dp[n-1],dp[n-2]);
+    }
+    
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        return solve(cost,n);
+    }
+};
